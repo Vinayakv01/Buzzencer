0d6b0ac55cd86a5bb6ae9672fae8591716a9dc16
Vinayakv01 <vinayakv949@gmail.com>
2024-03-06 10:37:57 +0530

    second push

--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Influencer from '../influencer/influencer';
 import Home from '../home/home';
+import Login from '../login/login';
+import Signup from '../login/signup';
 
 import {
     BrowserRouter, Routes, Route,
@@ -8,6 +10,8 @@ import {
 const routes = () => {
   return (
     <Routes>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/signin" element={<Login/>}/>
         <Route path="/Influencer" element={<Influencer/>}/>
         <Route path="/" element={<Home/>}/>
     </Routes>
