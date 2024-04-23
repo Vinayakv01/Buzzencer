d00a43dd7ea4e164f58567129546d28b87844a5a
Vinayakv01 <vinayakv949@gmail.com>
2024-04-23 11:42:24 +0530

    aajka

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -11,11 +11,12 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="py-4 px-8 container mx-auto bg-transparent">
-      <div className=" flex justify-between items-center">
+    <nav className={`py-4 px-8 container  ${location.pathname === '/contact' ? 'bg-white' : 'bg-transparent'}`}>
+      <div className="mx-8">
+      <div className="flex justify-between items-center">
         <div className="flex items-center space-x-2">
           <Link to="/">
-          <img src={buzzencer_logo} alt="G Logo" className="h-9" />
+            <img src={buzzencer_logo} alt="G Logo" className="h-9" />
           </Link>
         </div>
 
@@ -75,8 +76,96 @@ const NavBar = () => {
           </span>
         </div>
       )}
+      </div>
     </nav>
   );
 };
 
 export default NavBar;
+
+
+
+
+
+// import React, { useState } from 'react';
+// import buzzencer_logo from '../../assets/buzzencer_logo.svg';
+// import { Link, useLocation } from 'react-router-dom';
+
+// const NavBar = () => {
+//   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+//   const location = useLocation();
+
+//   const toggleMobileMenu = () => {
+//     setMobileMenuOpen(!isMobileMenuOpen);
+//   };
+
+//   return (
+//     <nav className="py-4 px-8 container mx-auto bg-transparent">
+//       <div className=" flex justify-between items-center">
+//         <div className="flex items-center space-x-2">
+//           <Link to="/">
+//           <img src={buzzencer_logo} alt="G Logo" className="h-9" />
+//           </Link>
+//         </div>
+
+//         <div className="block lg:hidden">
+//           <button
+//             onClick={toggleMobileMenu}
+//             className="text-[#403bbf] focus:outline-none"
+//           >
+//             &#9776;
+//           </button>
+//         </div>
+
+//         <div className="hidden lg:flex space-x-5 items-center">
+//           <div className="space-x-6 font-Poppins  flex items-center">
+//             <Link
+//               to="/"
+//               className={` ${location.pathname === '/' ? 'text-blue-600' : 'text-black'}  hover:text-gray-500`}
+//             >
+//               Home
+//             </Link>
+//             <Link
+//               to="/about"
+//               className={` ${location.pathname === '/about' ? 'text-blue-600' : 'text-black'} hover:text-gray-500`}
+//             >
+//               About us
+//             </Link>
+//             <Link
+//               to="/contact"
+//               className={` ${location.pathname === '/contact' ? 'text-blue-600' : 'text-black'} hover:text-gray-500`}
+//             >
+//               Contact Us
+//             </Link>
+//           </div>
+
+//           <Link
+//             to="/signin"
+//             className="text-white text-sm font-[650] font-Poppins px-9 pt-2.5 pb-3  bg-[#403bbf] rounded-full hover:bg-opacity-75 transition duration-300"
+//           >
+//             Sign-in
+//           </Link>
+//         </div>
+//       </div>
+
+//       {isMobileMenuOpen && (
+//         <div className="lg:hidden bg-[#403bbf] rounded-xl font-Poppins px-6 py-4 mt-2 space-y-2">
+//           <Link to="/" className={`text-white block  ${location.pathname === '/' ? 'hover:text-gray-500' : ''}`}>
+//             Home
+//           </Link>
+//           <Link to="/about" className={`text-white block  ${location.pathname === '/about' ? 'hover:text-gray-500' : ''}`}>
+//             About us
+//           </Link>
+//           <Link to="/contact" className={`text-white block  ${location.pathname === '/contact' ? 'hover:text-gray-500' : ''}`}>
+//             Contact Us
+//           </Link>
+//           <span className="flex justify-end">
+//           <Link to="/signin" className="text-white text-sm font-[650] font-Poppins px-9 pt-2.5 pb-3 bg-[rgba(1,1,1,0.1)] rounded-full ">Login</Link>
+//           </span>
+//         </div>
+//       )}
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -1,13 +1,86 @@
 import React from 'react'
+import loginImg from "../../assets/login.svg";
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
-    <div>
-      <p>
-        this is Contact
-      </p>
-    </div>
-  )
-}
+    <div className="md:flex h-screen">
+      {/* Left side (Image) */}
 
-export default Contact
+      <div className="w-1/2 bg-[#413BBE] flex items-center justify-center">
+      </div>
+
+      {/* Right side (Form) */}
+      <div className="md:w-1/2 w-full flex items-center justify-center">
+        <form className="w-3/4  p-8 ">
+         <div className="flex gap-4">
+          <div className="mb-4 font-Poppins">
+            <label htmlFor="username" className="block text-gray-600 mb-2 text-sm font-semibold ">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="w-full border px-6 pt-2 pb-2.5 rounded-full"
+              placeholder="Enter User Name"
+            />
+          </div>
+          <div className="mb-4 font-Poppins">
+            <label htmlFor="password" className="block text-gray-600 mb-2 text-sm font-semibold ">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border px-6 pt-2 pb-2.5 rounded-full"
+              placeholder="Enter Password"
+            />
+          </div>
+          </div>
+
+          <div className="mb-4 font-Poppins">
+            <label htmlFor="password" className="block text-gray-600 mb-2 text-sm font-semibold ">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border px-6 pt-2 pb-2.5 rounded-full"
+              placeholder="Enter Password"
+            />
+          </div>
+        <div className="mb-10 font-Montserrat">
+          
+          <label htmlFor="message" className="block text-gray-600 font-semibold  mb-2">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="6"
+            className="shadow-sm  mt-1  w-full sm:text-sm border  rounded-3xl p-3"
+            placeholder="Message"
+            style={{ overflow: 'auto', resize: 'none' }}            
+          ></textarea>
+          
+        </div>
+       
+        <span className="flex flex-row justify-center ">
+         <button
+            type="submit"
+            className="text-white text-sm font-[650] font-Poppins px-9 pt-2.5 pb-3  bg-[#403bbf] rounded-full hover:bg-opacity-75 transition duration-300"
+          >
+            Login
+          </button>
+          </span>
+      
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact 
